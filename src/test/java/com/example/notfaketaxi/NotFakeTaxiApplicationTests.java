1824fe11ddf253b99d879d5948929e8f70f62622
Viktor Kolev <viksuper555@gmail.com>
2021-11-23 23:04:47 +0200

    Initial commit

---
+++ src/test/java/com/example/notfaketaxi/NotFakeTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notfaketaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotFakeTaxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
