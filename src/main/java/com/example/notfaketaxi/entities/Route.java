6e136fe42cdfa17476592c1e1a5051933b17b841
donceci2001 <ceckopavlov@gmail.com>
2022-01-09 13:43:32 +0200

    hidden api key check scratch package, gitignor file row 21 errors in class HttpRoute with APOD and JsonBodyHandler if not necessary remove class Route or GoogleApiResponse.java

---
+++ src/main/java/com/example/notfaketaxi/entities/Route.java
@@ -0,0 +1,54 @@
+package com.example.notfaketaxi.entities;
+
+import org.springframework.data.mapping.KPropertyPathExtensionsKt;
+
+import java.awt.*;
+
+public class Route {
+
+    private double kilometers;
+    private double price;
+    //private Client driver;
+    //private Client clientName;
+    private Point initialLoc;
+    private Point finalLoc;
+
+    public double getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(double kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Point getInitialLoc() {
+        return initialLoc;
+    }
+
+    public void setInitialLoc(Point initialLoc) {
+        this.initialLoc = initialLoc;
+    }
+
+    public Point getFinalLoc() {
+        return finalLoc;
+    }
+
+    public void setFinalLoc(Point finalLoc) {
+        this.finalLoc = finalLoc;
+    }
+
+    public Route(double kilometers, double price, Point initialLoc, Point finalLoc) {
+        this.kilometers = kilometers;
+        this.price = price;
+        this.initialLoc = initialLoc;
+        this.finalLoc = finalLoc;
+    }
+}
